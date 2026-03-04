82b9c2934b2ac2bb077d59883ea1173da7bf60d7
Jefry1106 <yefrygualteros1106@gmail.com>
2026-03-03 21:03:03 -0500

    Fix JWT security configuration 2

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 
 const app = require("./app");
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
 
-console.log("Servidor corriendo en puerto 3000");
+    console.log("Servidor corriendo en puerto 3000");
 
 });
